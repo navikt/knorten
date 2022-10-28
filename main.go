78d4bc89061ee1bf985a2f4ebf4213b38ebfb4df
Erik Vattekar <erik.vattekar@nav.no>
2022-10-28 14:10:02 +0200

    helm upgrade example
    Co-authored-by: Kyrre Havik <Kyrremann@gmail.com>

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/cli"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
@@ -12,32 +14,6 @@ import (
 func main() {
 	// Hello
 	// 1. Rulle ut helm-chart med values
-	//airflowEntry := &helmRepo.Entry{
-	//	Name: "Airflow",
-	//	URL:  "",
-	//}
-	//airflowProviders := getter.Providers{
-	//	{
-	//		Schemes: nil,
-	//		New:     nil,
-	//	},
-	//}
-	//
-	//chartRepository, err := helmRepo.NewChartRepository(airflowEntry, airflowProviders)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//airflowConfig := helmAction.Configuration{
-	//	RESTClientGetter: nil,
-	//	Releases:         nil,
-	//	KubeClient:       nil,
-	//	RegistryClient:   nil,
-	//	Capabilities:     nil,
-	//	Log:              nil,
-	//}
-	//helmAction.NewUpgrade()
-
 	settings := cli.New()
 	settings.SetNamespace("team-nada")
 
@@ -61,4 +37,36 @@ func main() {
 	for _, rel := range results {
 		log.Printf("%+v", rel.Name)
 	}
+
+	destDir := "/tmp"
+	chartName := "jupyterhub"
+	version := "0.11.1"
+	pullClient := action.NewPullWithOpts(action.WithConfig(actionConfig))
+	pullClient.RepoURL = "https://jupyterhub.github.io/helm-chart"
+	pullClient.Settings = settings
+	pullClient.DestDir = destDir
+	pullClient.Version = version
+	_, err = pullClient.Run(chartName)
+	if err != nil {
+		log.Panicf("failed to pull chart: %v, error: %v", chartName, err)
+	}
+
+	chart, err := loader.Load(fmt.Sprintf("%v/%v-%v.tgz", destDir, chartName, version))
+	upgradeClient := action.NewUpgrade(actionConfig)
+	upgradeClient.Namespace = "team-nada"
+	upgradeClient.Install = true
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := chart.Values["proxy"].(map[string]interface{})
+	proxy["secretToken"] = ""
+	chart.Values["proxy"] = proxy
+
+	release, err := upgradeClient.Run("jlab", chart, chart.Values)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(release.Info)
 }

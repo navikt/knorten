4df3260ef5e50692e18c249c4bca5a876308c499
Kyrre Havik <kyrremann@gmail.com>
2022-11-14 08:57:18 +0100

    feat: oppdatert team og user siden

--- pkg/api/team.go
+++ pkg/api/team.go
@@ -11,34 +11,45 @@ import (
 func (a *API) setupTeamRoutes() {
 	a.router.GET("/team/new", func(c *gin.Context) {
 		var form team.Form
-		c.HTML(http.StatusOK, "charts/team.tmpl", gin.H{
-			"form": form,
+		services, err := createServiceSidebar(c, a.repo)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.HTML(http.StatusOK, "team/new", gin.H{
+			"form":     form,
+			"services": services,
 		})
 	})
 
 	a.router.POST("/team/new", func(c *gin.Context) {
 		err := team.Create(c, a.repo, a.googleClient, a.k8sClient)
 		if err != nil {
+			// TODO: Bruke middleware/session for å legge feilmeldinger til http-kallet
+			// c.Error()
 			fmt.Println(err)
 			// c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			c.Redirect(http.StatusSeeOther, "/team/new")
+			return
 		}
 		c.Redirect(http.StatusSeeOther, "/user")
 	})
 
-	//a.router.GET("/team/:team/edit", func(c *gin.Context) {
-	//	teamName := c.Param("team")
-	//	namespaceForm := &team.Form{}
-	//	err := a.repo.TeamConfigurableValuesGet(c, gensql.ChartTypeNamespace, teamName, namespaceForm)
-	//	if err != nil {
-	//		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	//		return
-	//	}
-	//	c.HTML(http.StatusOK, "charts/team.tmpl", gin.H{
-	//		"values": namespaceForm,
-	//		"team":   teamName,
-	//	})
-	//})
+	a.router.GET("/team/:team/edit", func(c *gin.Context) {
+		teamName := c.Param("team")
+		get, err := a.repo.TeamGet(c, teamName)
+		if err != nil {
+			fmt.Println(err)
+			// c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.Redirect(http.StatusSeeOther, "/team/new")
+			return
+		}
+		c.HTML(http.StatusOK, "team/edit", gin.H{
+			"users": get.Users,
+			"team":  teamName,
+		})
+	})
 
 	//a.router.POST("/team/:team/edit", func(c *gin.Context) {
 	//	err := chart.UpdateNamespace(c, a.helmClient, a.repo)
--- pkg/api/user.go
+++ pkg/api/user.go
@@ -4,9 +4,23 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/nais/knorten/pkg/auth"
+	"github.com/nais/knorten/pkg/database"
 	"github.com/nais/knorten/pkg/database/gensql"
 	"net/http"
 )
+
+type Service struct {
+	App            string
+	Ingress        string
+	Namespace      string
+	Secret         string
+	ServiceAccount string
+}
+
+type Services struct {
+	Jupyterhub *Service
+	Airflow    *Service
+}
 
 func createIngress(team string, chartType gensql.ChartType) string {
 	switch chartType {
@@ -19,40 +33,55 @@ func createIngress(team string, chartType gensql.ChartType) string {
 	return ""
 }
 
+func createService(team string, chartType gensql.ChartType) *Service {
+	return &Service{
+		App:            string(chartType),
+		Ingress:        createIngress(team, chartType),
+		Namespace:      team,
+		Secret:         fmt.Sprintf("https://console.cloud.google.com/security/secret-manager/secret/%v/versions?project=knada-gcp", team),
+		ServiceAccount: fmt.Sprintf("%v@knada-gcp.iam.gserviceaccount.com", team),
+	}
+}
+
+func createServiceSidebar(c *gin.Context, repo *database.Repo) (map[string]Services, error) {
+	var user *auth.User
+	anyUser, exists := c.Get("user")
+	if exists {
+		user = anyUser.(*auth.User)
+	}
+
+	get, err := repo.ServicesForUser(c, user.Email)
+	if err != nil {
+		return map[string]Services{}, err
+	}
+
+	services := map[string]Services{}
+	for team, apps := range get {
+		service := Services{}
+		for _, app := range apps {
+			switch app {
+			case gensql.ChartTypeJupyterhub:
+				service.Jupyterhub = createService(team, app)
+			case gensql.ChartTypeAirflow:
+				service.Airflow = createService(team, app)
+			}
+		}
+		services[team] = service
+	}
+
+	return services, nil
+}
+
 func (a *API) setupUserRoutes() {
 	a.router.GET("/user", func(c *gin.Context) {
-		var user *auth.User
-		anyUser, exists := c.Get("user")
-		if exists {
-			user = anyUser.(*auth.User)
-		}
-
-		fmt.Println(user)
-		get, err := a.repo.ServicesForUser(c, user.Email)
+		services, err := createServiceSidebar(c, a.repo)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		fmt.Println(get)
-		services := map[string][]Service{}
-		for team, apps := range get {
-			fmt.Println(team, apps)
-			services[team] = []Service{}
-			for _, app := range apps {
-				services[team] = append(services[team], Service{
-					App:            string(app),
-					Ingress:        createIngress(team, app),
-					Namespace:      team,
-					Secret:         fmt.Sprintf("https://console.cloud.google.com/security/secret-manager/secret/%v/versions?project=knada-gcp", team),
-					ServiceAccount: fmt.Sprintf("%v@knada-gcp.iam.gserviceaccount.com", team),
-				})
-			}
-		}
-		c.HTML(http.StatusOK, "user/index.tmpl", gin.H{
-			"current": "user",
-			"user":    c.Param("logged in user"),
-			"charts":  services,
+		c.HTML(http.StatusOK, "user/index", gin.H{
+			"services": services,
 		})
 	})
 }

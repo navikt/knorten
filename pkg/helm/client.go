65f9350e918a50184e5b36c1754a5b12a073e4e0
Kyrre Havik <kyrremann@gmail.com>
2023-01-23 15:51:46 +0100

    fix: fungerte ikke

--- pkg/helm/client.go
+++ pkg/helm/client.go
@@ -49,7 +49,6 @@ func (c *Client) InstallOrUpgrade(releaseName, chartVersion, namespace string, v
 	settings := cli.New()
 	settings.SetNamespace(namespace)
 	actionConfig := new(action.Configuration)
-	actionConfig.Log = c.log.Printf
 	if err := actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), "secret", log.Printf); err != nil {
 		c.log.WithError(err).Errorf("error while init actionConfig for %v", releaseName)
 		return err

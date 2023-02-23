c90532c7d00cdcfa841b50884641fb623c2c8b6f
Kyrre Havik <kyrremann@gmail.com>
2023-02-23 15:50:06 +0100

    fix: feilet fordi vi prøvde å logge inn

--- pkg/api/auth.go
+++ pkg/api/auth.go
@@ -261,6 +261,7 @@ func (a *API) setupAuthRoutes() {
 	a.router.GET("/oauth2/login", func(c *gin.Context) {
 		if a.dryRun {
 			c.Redirect(http.StatusSeeOther, "http://localhost:8080/user")
+			return
 		}
 
 		consentURL := a.login(c)

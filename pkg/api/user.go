09ae96a0edfe82e2013bb71136e1066b55f82fbe
Kyrre Havik <kyrremann@gmail.com>
2023-02-23 15:01:46 +0100

    fix: viser flashes på user-siden

--- pkg/api/user.go
+++ pkg/api/user.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/nais/knorten/pkg/auth"
 	"net/http"
@@ -14,9 +15,18 @@ func (a *API) setupUserRoutes() {
 			user = anyUser.(*auth.User)
 		}
 
+		session := sessions.Default(c)
+		flashes := session.Flashes()
+		err := session.Save()
+		if err != nil {
+			a.log.WithError(err).Error("problem saving session")
+			return
+		}
+
 		services, err := a.repo.ServicesForUser(c, user.Email)
 		c.HTML(http.StatusOK, "user/index", gin.H{
 			"errors":   err,
+			"flashes":  flashes,
 			"services": services,
 		})
 	})

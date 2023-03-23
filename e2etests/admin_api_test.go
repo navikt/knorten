24a17ac046ebc5b4c0bacffa3383c8d4354258db
Erik Vattekar <erik.vattekar@nav.no>
2023-03-23 10:49:21 +0100

    simple test for admin

--- e2etests/admin_api_test.go
+++ e2etests/admin_api_test.go
@@ -1,14 +1,18 @@
 package e2etests
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"testing"
+
+	"github.com/nais/knorten/pkg/database/gensql"
 )
 
 func TestAdminAPI(t *testing.T) {
 	teamName := "admintest"
+	ctx := context.Background()
 
 	t.Run("get admin html", func(t *testing.T) {
 		resp, err := server.Client().Get(fmt.Sprintf("%v/admin", server.URL))
@@ -35,8 +39,7 @@ func TestAdminAPI(t *testing.T) {
 		}
 
 		expected, err := createExpectedHTML("admin/index", map[string]any{
-			"current": "admin",
-			"teams":   map[string]any{},
+			"teams": map[string]any{},
 		})
 		if err != nil {
 			t.Fatal(err)
@@ -55,7 +58,188 @@ func TestAdminAPI(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	t.Run("get admin html after team and app creation", func(t *testing.T) {
+		resp, err := server.Client().Get(fmt.Sprintf("%v/admin", server.URL))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Status code is %v, should be %v", resp.StatusCode, http.StatusOK)
+		}
+
+		if resp.Header.Get("Content-Type") != htmlContentType {
+			t.Fatalf("Content-Type header is %v, should be %v", resp.Header.Get("Content-Type"), htmlContentType)
+		}
+
+		received, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		receivedMinimized, err := minimizeHTML(string(received))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		team, err := repo.TeamGet(ctx, teamName)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected, err := createExpectedHTML("admin/index", map[string]any{
+			"teams": map[string]any{
+				team.ID: map[string]any{
+					"ID":    team.ID,
+					"Slug":  teamName,
+					"Users": []string{"dummy@nav.no"},
+					"Apps":  []string{"jupyterhub", "airflow"},
+				},
+			},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		expectedMinimized, err := minimizeHTML(expected)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if receivedMinimized != string(expectedMinimized) {
+			t.Fatal("Received and expected HTML response are different")
+		}
+	})
+
+	t.Run("get jupyterhub values html", func(t *testing.T) {
+		resp, err := server.Client().Get(fmt.Sprintf("%v/admin/jupyterhub", server.URL))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Status code is %v, should be %v", resp.StatusCode, http.StatusOK)
+		}
+
+		if resp.Header.Get("Content-Type") != htmlContentType {
+			t.Fatalf("Content-Type header is %v, should be %v", resp.Header.Get("Content-Type"), htmlContentType)
+		}
+
+		received, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		receivedMinimized, err := minimizeHTML(string(received))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected, err := createExpectedHTML("admin/chart", map[string]any{
+			"chart":  gensql.ChartTypeJupyterhub,
+			"values": []gensql.ChartGlobalValue{},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		expectedMinimized, err := minimizeHTML(expected)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fmt.Println(string(received))
+		fmt.Println("-------------------------")
+		fmt.Println(expected)
+
+		if receivedMinimized != string(expectedMinimized) {
+			t.Fatal("Received and expected HTML response are different")
+		}
+	})
+
+	exisitingJupyterGlobals := map[string]string{
+		"global.key1": "value",
+		"global.key2": "value",
+	}
+	if err := createJupyterGlobalValues(ctx, exisitingJupyterGlobals); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("get jupyterhub values html adding", func(t *testing.T) {
+		resp, err := server.Client().Get(fmt.Sprintf("%v/admin/jupyterhub", server.URL))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Status code is %v, should be %v", resp.StatusCode, http.StatusOK)
+		}
+
+		if resp.Header.Get("Content-Type") != htmlContentType {
+			t.Fatalf("Content-Type header is %v, should be %v", resp.Header.Get("Content-Type"), htmlContentType)
+		}
+
+		received, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		receivedMinimized, err := minimizeHTML(string(received))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected, err := createExpectedHTML("admin/chart", map[string]any{
+			"chart": gensql.ChartTypeJupyterhub,
+			"values": []gensql.ChartGlobalValue{
+				{
+					Key:   "global.key1",
+					Value: "value",
+				},
+				{
+					Key:   "global.key2",
+					Value: "value",
+				},
+			},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		expectedMinimized, err := minimizeHTML(expected)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if receivedMinimized != string(expectedMinimized) {
+			t.Fatal("Received and expected HTML response are different")
+		}
+	})
+
+	if err := deleteJupyterGlobalValues(ctx, exisitingJupyterGlobals); err != nil {
+		t.Fatal(err)
+	}
+
 	if err := cleanupTeamAndApps(teamName); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func createJupyterGlobalValues(ctx context.Context, values map[string]string) error {
+	for k, v := range values {
+		if err := repo.GlobalChartValueInsert(ctx, k, v, false, gensql.ChartTypeJupyterhub); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func deleteJupyterGlobalValues(ctx context.Context, values map[string]string) error {
+	for k := range values {
+		if err := repo.GlobalValueDelete(ctx, k, gensql.ChartTypeJupyterhub); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }

3cb38e9438af87624a64a4dad6249e7953a29ecb
Kyrre Havik <kyrremann@gmail.com>
2023-07-12 15:32:35 +0200

    feat: eventlog bruker fields for info om eventen
    Slettet også Fatal og Debug da de foreløpig ikke er i bruk.

--- pkg/events/eventlogs.go
+++ pkg/events/eventlogs.go
@@ -6,76 +6,59 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/nais/knorten/pkg/database/gensql"
+	"github.com/sirupsen/logrus"
 )
 
 type EventLogger struct {
-	EventID uuid.UUID
+	eventID uuid.UUID
+	log     *logrus.Entry
 }
 
 func (el EventLogger) Infof(template string, arg ...any) {
 	message := fmt.Sprintf(template, arg...)
-	log.Info(message)
+	el.log.Info(message)
+
 	err := dbQuerier.EventLogCreate(eventContext, gensql.EventLogCreateParams{
-		EventID: el.EventID,
+		EventID: el.eventID,
 		Message: message,
 		LogType: gensql.LogTypeInfo,
 	})
 	if err != nil {
-		log.Errorf("can't write event(%v) to database: %v", el.EventID, err)
+		el.log.WithError(err).Error("can't write event to database")
 	}
 }
 
-func (el EventLogger) Errorf(messageTemplate string, arg ...any) {
-	message := fmt.Sprintf(messageTemplate, arg...)
-	log.Error(message)
+func (el EventLogger) Errorf(template string, arg ...any) {
+	message := fmt.Sprintf(template, arg...)
+	el.log.Error(message)
+
 	err := dbQuerier.EventLogCreate(eventContext, gensql.EventLogCreateParams{
-		EventID: el.EventID,
+		EventID: el.eventID,
 		Message: message,
 		LogType: gensql.LogTypeError,
 	})
 	if err != nil {
-		log.Errorf("can't write event(%v) to database: %v", el.EventID, err)
+		el.log.WithError(err).Error("can't write event to database")
 	}
+
 	err = dbQuerier.EventSetDeadline(eventContext, gensql.EventSetDeadlineParams{
 		Deadline: time.Now().Add(3 * time.Minute),
 	})
 	if err != nil {
-		log.Errorf("can't extend the deadline for event(%v) in database: %v", el.EventID, err)
+		el.log.WithError(err).Errorf("can't extend the deadline for event")
 	}
+
 	err = dbQuerier.EventSetStatus(eventContext, gensql.EventSetStatusParams{
 		Status: gensql.EventStatusPending,
 	})
 	if err != nil {
-		log.Errorf("can't set status to %v for event(%v) in database: %v", gensql.EventStatusPending, el.EventID, err)
+		el.log.WithError(err).Errorf("can't set status to %v for event", gensql.EventStatusPending)
 	}
 }
 
-func (el EventLogger) Fatalf(messageTemplate string, arg ...any) {
-	message := fmt.Sprintf(messageTemplate, arg...)
-	err := dbQuerier.EventLogCreate(eventContext, gensql.EventLogCreateParams{
-		EventID: el.EventID,
-		Message: message,
-		LogType: gensql.LogTypeFatal,
-	})
-	if err != nil {
-		log.Errorf("can't write event(%v) to database: %v", el.EventID, err)
-	}
-	err = dbQuerier.EventSetStatus(eventContext, gensql.EventSetStatusParams{
-		Status: gensql.EventStatusFailed,
-	})
-	if err != nil {
-		log.Errorf("can't set status to %v for event(%v) in database: %v", gensql.EventStatusFailed, el.EventID, err)
-	}
-	log.Fatal(message)
-}
-
-func (el EventLogger) Debugf(messageTemplate string, arg ...any) {
-	message := fmt.Sprintf(messageTemplate, arg...)
-	log.Info(message)
-}
-
-func newEventLogger(eventID uuid.UUID) EventLogger {
+func newEventLogger(event gensql.Event) EventLogger {
 	return EventLogger{
-		EventID: eventID,
+		eventID: event.ID,
+		log:     log.WithField("eventType", event.EventType).WithField("eventID", event.ID),
 	}
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,8 +1,6 @@
 package logger
 
 type Logger interface {
-	Fatalf(string, ...any)
 	Errorf(string, ...any)
 	Infof(string, ...any)
-	Debugf(string, ...any)
 }

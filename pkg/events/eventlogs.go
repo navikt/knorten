c13d68f44376c05837fc206661ead8fa83a752bc
Kyrre Havik <kyrremann@gmail.com>
2023-07-24 09:06:32 +0200

    fix: bruke Fields i loggene
    Når jeg returnerte logrus så sluttet vi å logge til databasen, så kan
    ikke ha et interface som passer med logrus.
    
    Merk at Fields ikke blir logget til databasen, som betyr at vi ikke
    logger err til brukeren. Tror det kan være en god ting.

--- pkg/events/eventlogs.go
+++ pkg/events/eventlogs.go
@@ -8,6 +8,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/nais/knorten/pkg/database"
 	"github.com/nais/knorten/pkg/database/gensql"
+	"github.com/nais/knorten/pkg/logger"
 	"github.com/sirupsen/logrus"
 )
 
@@ -34,27 +35,36 @@ func (e EventLogger) Infof(template string, arg ...any) {
 	e.Info(fmt.Sprintf(template, arg...))
 }
 
-func (e EventLogger) Errorf(template string, arg ...any) {
-	message := fmt.Sprintf(template, arg...)
-	e.log.Error(message)
+func (e EventLogger) Error(messages ...any) {
+	for _, message := range messages {
+		messageAsString := fmt.Sprint(message)
 
-	err := e.repo.EventLogCreate(e.context, e.eventID, message, gensql.LogTypeError)
-	if err != nil {
-		e.log.WithError(err).Error("can't write event to database")
+		e.log.Error(messageAsString)
+		fmt.Println(e.log.String())
+		err := e.repo.EventLogCreate(e.context, e.eventID, messageAsString, gensql.LogTypeError)
+		if err != nil {
+			e.log.WithError(err).Error("can't write event to database")
+		}
 	}
 
-	err = e.repo.EventSetDeadline(e.context, time.Now().Add(3*time.Minute))
+	err := e.repo.EventSetDeadline(e.context, time.Now().Add(3*time.Minute))
 	if err != nil {
 		e.log.WithError(err).Errorf("can't extend the deadline for event")
 	}
 }
 
-func (e EventLogger) WithField(key string, value interface{}) *logrus.Entry {
-	return e.log.WithFields(logrus.Fields{key: value})
+func (e EventLogger) Errorf(template string, arg ...any) {
+	e.Error(fmt.Sprintf(template, arg...))
 }
 
-func (e EventLogger) WithError(err error) *logrus.Entry {
-	return e.log.WithField(logrus.ErrorKey, err)
+func (e EventLogger) WithField(key string, value interface{}) logger.Logger {
+	e.log = e.log.WithFields(logrus.Fields{key: value})
+	return e
+}
+
+func (e EventLogger) WithError(err error) logger.Logger {
+	e.log = e.log.WithField(logrus.ErrorKey, err)
+	return e
 }
 
 func newEventLogger(ctx context.Context, log *logrus.Entry, repo *database.Repo, event gensql.Event) EventLogger {
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,12 +1,11 @@
 package logger
 
-import "github.com/sirupsen/logrus"
-
 type Logger interface {
 	Errorf(string, ...any)
+	Error(string ...any)
 	Infof(string, ...any)
 	Info(string ...any)
 
-	WithError(error) *logrus.Entry
-	WithField(string, any) *logrus.Entry
+	WithError(error) Logger
+	WithField(string, any) Logger
 }

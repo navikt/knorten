4268fe5a6fabe1f99ae61bb5b4b6515aeb039b65
Kyrre Havik <kyrremann@gmail.com>
2023-07-25 08:11:45 +0200

    feat: forenklet redigering av chart values

--- pkg/api/chart.go
+++ pkg/api/chart.go
@@ -153,60 +153,32 @@ func (c *client) setupChartRoutes() {
 	})
 
 	c.router.GET("/team/:team/:chart/edit", func(ctx *gin.Context) {
-		slug := ctx.Param("team")
+		team := ctx.Param("team")
 		chartType := getChartType(ctx.Param("chart"))
 
-		team, err := c.repo.TeamGet(ctx, slug)
-		if err != nil {
-			session := sessions.Default(ctx)
-			session.AddFlash(err.Error())
-			err := session.Save()
-			if err != nil {
-				c.log.WithError(err).Error("problem saving session")
-				ctx.Redirect(http.StatusSeeOther, "/oversikt")
-				return
-			}
-			ctx.Redirect(http.StatusSeeOther, "/oversikt")
-			return
-		}
-
-		var form any
-		var allowlist []string
-		switch chartType {
-		case gensql.ChartTypeJupyterhub:
-			form = &chart.JupyterConfigurableValues{}
-			allowlist, err = c.getExistingAllowlist(ctx, team.ID)
-			if err != nil {
-				if !errors.Is(err, sql.ErrNoRows) {
-					c.log.WithError(err).Error("fetching existing jupyterhub allowlist")
-					ctx.Redirect(http.StatusSeeOther, "/oversikt")
-				}
-			}
-		case gensql.ChartTypeAirflow:
-			form = &chart.AirflowConfigurableValues{}
-		default:
-			ctx.JSON(http.StatusBadRequest, map[string]string{
-				"status":  strconv.Itoa(http.StatusBadRequest),
-				"message": fmt.Sprintf("Chart type %v is not supported", chartType),
-			})
-			return
-		}
-
-		err = c.repo.TeamConfigurableValuesGet(ctx, chartType, team.ID, form)
-		if err != nil {
-			session := sessions.Default(ctx)
-			session.AddFlash(err.Error())
-			err := session.Save()
-			if err != nil {
-				c.log.WithError(err).Error("problem saving session")
-				ctx.Redirect(http.StatusSeeOther, "/oversikt")
-				return
-			}
-			ctx.Redirect(http.StatusSeeOther, "/oversikt")
-			return
-		}
-
 		session := sessions.Default(ctx)
+
+		form, err := c.getEditChart(ctx, team, chartType)
+		if err != nil {
+			var validationErrorse validator.ValidationErrors
+			if errors.As(err, &validationErrorse) {
+				for _, fieldError := range validationErrorse {
+					session.AddFlash(descriptiveMessageForChartError(fieldError))
+				}
+			} else {
+				session.AddFlash(err.Error())
+			}
+
+			err := session.Save()
+			if err != nil {
+				c.log.WithError(err).Error("problem saving session")
+				ctx.Redirect(http.StatusSeeOther, "/oversikt")
+				return
+			}
+			ctx.Redirect(http.StatusSeeOther, "/oversikt")
+			return
+		}
+
 		flashes := session.Flashes()
 		err = session.Save()
 		if err != nil {
@@ -215,13 +187,9 @@ func (c *client) setupChartRoutes() {
 		}
 
 		c.htmlResponseWrapper(ctx, http.StatusOK, fmt.Sprintf("charts/%v", chartType), gin.H{
-			"team":                  slug,
-			"pending_jupyterhub":    team.PendingJupyterUpgrade,
-			"pending_airflow":       team.PendingAirflowUpgrade,
-			"restrictairflowegress": team.RestrictAirflowEgress,
-			"allowlist":             allowlist,
-			"values":                form,
-			"errors":                flashes,
+			"team":   team,
+			"values": form,
+			"errors": flashes,
 		})
 	})
 
@@ -365,6 +333,57 @@ func (c *client) newChart(ctx *gin.Context, teamSlug string, chartType gensql.Ch
 	}
 
 	return fmt.Errorf("chart type %v is not supported", chartType)
+}
+
+func (c *client) getEditChart(ctx *gin.Context, slug string, chartType gensql.ChartType) (any, error) {
+	team, err := c.repo.TeamGet(ctx, slug)
+	if err != nil {
+		return nil, err
+	}
+
+	var chartObjects any
+	var allowlist []string
+	switch chartType {
+	case gensql.ChartTypeJupyterhub:
+		chartObjects = &chart.JupyterConfigurableValues{}
+		allowlist, err = c.getExistingAllowlist(ctx, team.ID)
+		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+			return nil, err
+		}
+	case gensql.ChartTypeAirflow:
+		chartObjects = &chart.AirflowConfigurableValues{}
+	default:
+		return nil, fmt.Errorf("chart type %v is not supported", chartType)
+	}
+
+	err = c.repo.TeamConfigurableValuesGet(ctx, chartType, team.ID, chartObjects)
+	if err != nil {
+		return nil, err
+	}
+
+	var form any
+	switch chartType {
+	case gensql.ChartTypeJupyterhub:
+		jupyterhubValues := chartObjects.(*chart.JupyterConfigurableValues)
+		form = JupyterForm{
+			CPU:         jupyterhubValues.CPU,
+			Memory:      jupyterhubValues.Memory,
+			ImageName:   jupyterhubValues.ImageName,
+			ImageTag:    jupyterhubValues.ImageTag,
+			CullTimeout: jupyterhubValues.CullTimeout,
+			Allowlist:   allowlist,
+		}
+	case gensql.ChartTypeAirflow:
+		airflowValues := chartObjects.(*chart.AirflowConfigurableValues)
+		form = AirflowForm{
+			DagRepo:        airflowValues.DagRepo,
+			DagRepoBranch:  airflowValues.DagRepoBranch,
+			ApiAccess:      strconv.FormatBool(team.ApiAccess),
+			RestrictEgress: strconv.FormatBool(team.RestrictAirflowEgress),
+		}
+	}
+
+	return form, nil
 }
 
 func (c *client) editChart(ctx *gin.Context, teamSlug string, chartType gensql.ChartType) error {

40dbffbb0df7a81ac86d9c0df37769ae973e5b8b
Erik Vattekar <erik.vattekar@nav.no>
2023-11-27 12:02:19 +0100

    remove unused functions and constants

--- pkg/team/k8s.go
+++ pkg/team/k8s.go
@@ -7,12 +7,6 @@ import (
 	v1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-)
-
-const (
-	k8sLabelEnableTeamNetworkPolicies = "team-netpols"
-	replicatorLabel                   = "app.kubernetes.io/managed-by=replicator"
 )
 
 func (c Client) k8sNamespaceExists(ctx context.Context, namespace string) (bool, error) {
@@ -102,54 +96,6 @@ func (c Client) createK8sServiceAccount(ctx context.Context, teamID, namespace s
 	_, err := c.k8sClient.CoreV1().ServiceAccounts(namespace).Create(ctx, saSpec, metav1.CreateOptions{})
 	if err != nil && !k8sErrors.IsAlreadyExists(err) {
 		return err
-	}
-
-	return nil
-}
-
-func (c Client) removeReplicatorNetpols(ctx context.Context, namespace string) error {
-	if err := c.removeFQDNNetpols(ctx, namespace); err != nil {
-		return err
-	}
-
-	return c.removeRegularNetpols(ctx, namespace)
-}
-
-func (c Client) removeFQDNNetpols(ctx context.Context, namespace string) error {
-	fqdnNetpolResource := schema.GroupVersionResource{
-		Group:    "networking.gke.io",
-		Version:  "v1alpha3",
-		Resource: "fqdnnetworkpolicies",
-	}
-	fqdnNetpols, err := c.k8sDynamicClient.Resource(fqdnNetpolResource).Namespace(namespace).List(ctx, metav1.ListOptions{
-		LabelSelector: replicatorLabel,
-	})
-	if err != nil {
-		return err
-	}
-
-	for _, fqdn := range fqdnNetpols.Items {
-		if err := c.k8sDynamicClient.Resource(fqdnNetpolResource).Namespace(namespace).Delete(ctx, fqdn.GetName(), metav1.DeleteOptions{}); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (c Client) removeRegularNetpols(ctx context.Context, namespace string) error {
-	netpols, err := c.k8sClient.NetworkingV1().NetworkPolicies(namespace).List(ctx, metav1.ListOptions{
-		LabelSelector: replicatorLabel,
-	})
-	if err != nil {
-		return err
-	}
-
-	for _, netpol := range netpols.Items {
-		err := c.k8sClient.NetworkingV1().NetworkPolicies(namespace).Delete(ctx, netpol.Name, metav1.DeleteOptions{})
-		if err != nil && !k8sErrors.IsNotFound(err) {
-			return err
-		}
 	}
 
 	return nil

c679c15c3e3f924d739a716b22efd9318fbeb7a5
Paul B. Beskow <paul@beskow.no>
2024-02-16 09:13:09 +0100

    chore(manifest): move secret manifest

---
+++ pkg/k8s/core/core.go
@@ -0,0 +1,16 @@
+package core
+
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func NewSecret(name, namespace string, data map[string]string) *v1.Secret {
+	return &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		StringData: data,
+	}
+}

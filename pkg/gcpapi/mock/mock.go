3f769e708196e73e430c4320779f24a6d0ccd32a
Paul B. Beskow <paul@beskow.no>
2024-02-22 13:00:23 +0100

    chore(gcpapi): simplify interfaces

--- pkg/gcpapi/mock/mock.go
+++ pkg/gcpapi/mock/mock.go
@@ -6,16 +6,16 @@ import (
 )
 
 type ServiceAccountPolicyManager struct {
-	GetPolicyFunc func(ctx context.Context, resource string) (*iam.Policy, error)
-	SetPolicyFunc func(ctx context.Context, resource string, policy *iam.Policy) (*iam.Policy, error)
+	GetPolicyFunc func(ctx context.Context, name string) (*iam.Policy, error)
+	SetPolicyFunc func(ctx context.Context, name string, policy *iam.Policy) (*iam.Policy, error)
 }
 
-func (m *ServiceAccountPolicyManager) GetPolicy(ctx context.Context, resource string) (*iam.Policy, error) {
-	return m.GetPolicyFunc(ctx, resource)
+func (m *ServiceAccountPolicyManager) GetPolicy(ctx context.Context, name string) (*iam.Policy, error) {
+	return m.GetPolicyFunc(ctx, name)
 }
 
-func (m *ServiceAccountPolicyManager) SetPolicy(ctx context.Context, resource string, policy *iam.Policy) (*iam.Policy, error) {
-	return m.SetPolicyFunc(ctx, resource, policy)
+func (m *ServiceAccountPolicyManager) SetPolicy(ctx context.Context, name string, policy *iam.Policy) (*iam.Policy, error) {
+	return m.SetPolicyFunc(ctx, name, policy)
 }
 
 func NewServiceAccountPolicyManager(policy *iam.Policy, err error) *ServiceAccountPolicyManager {

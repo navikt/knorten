b9ec2e6855e2c5cf5adb42096c658a7c31daf8a2
Erik Vattekar <erik.vattekar@nav.no>
2024-05-29 13:56:43 +0200

    correct secret store kind

--- pkg/events/dispatcher.go
+++ pkg/events/dispatcher.go
@@ -403,6 +403,7 @@ func createExternalSecret(secrets []secrets.TeamSecret, teamID, secretGroup stri
 				Name: secretGroup,
 			},
 			SecretStoreRef: v1alpha1.SecretStoreRef{
+				Kind: v1alpha1.ClusterSecretStoreKind,
 				Name: "default-gsm-store",
 			},
 		},

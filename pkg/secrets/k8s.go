31d63ac4201711b3bcef33dc9be153fc728068e9
Erik Vattekar <erik.vattekar@nav.no>
2024-06-05 18:13:55 +0200

    remove early return used for testing locally

--- pkg/secrets/k8s.go
+++ pkg/secrets/k8s.go
@@ -12,7 +12,6 @@ import (
 )
 
 func (e *ExternalSecretClient) ApplyExternalSecret(ctx context.Context, teamID, secretGroup string) error {
-	return nil
 	secrets, err := e.GetTeamSecretGroup(ctx, nil, teamID, secretGroup)
 	if err != nil {
 		return err

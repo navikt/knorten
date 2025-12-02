9cc433ce6dfd832bb754ac760481cb06e2b310df
Petter Jacobsen <petter.jacobsen@nav.no>
2025-12-02 09:45:25 +0100

    chore: upgrade to remove glob severity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.tmpl"],
+  content: ["./templates/**/*.tmpl"],
+  defaultExtractor: content => content.match(/[\w\-:.\/\[#%\]]+(?<!:)/g) || [],
   theme: {
     fontFamily: {
       "font-family": "\"Source Sans Pro\", Arial, sans-serif"
